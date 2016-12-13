3653d7eb9b010ec78964083ddd2ab34b0e771530
Tzuwen <u9541526@gmail.com>
2016-12-13 20:20:51 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var routes = require('./api/routes');
+
+app.set('port', 3000);
+
+app.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/api', routes);
+
+app.get('/file', function(req, res) {
+    console.log("Get the file");
+    res
+        .status(200)
+        .sendFile(path.join(__dirname, 'app.js'));
+});
+
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port;
+    console.log("listen to port " + port);
+});
---
+++ readme.md
@@ -0,0 +1,2 @@
+# MEAN stack app
+This repo is a simple MEAN stack app demo# mean-stack-app
